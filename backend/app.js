5a42c075fcee0f969c298dad580b9605c636584c
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2017-11-02 16:39:46 +0800

    Fixed the error.

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,57 @@
 const express = require('express');
 const reload = require('reload');
 const watch = require('watch');
+const bodyParser = require("body-parser");  
+const jwt = require('jwt-simple');
+const auth = require('./auth')();
+const config = require('./config');
+const users = require('./users');
+
+
 const app = express();
 
-app.get('/api',(req,res)=>{
-    res.json({"key":"Hello World!"});
+
+
+app.use(bodyParser.json());
+app.use(auth.initialize());
+
+app.get('/api/data',auth.authenticate(),(req,res)=>{
+    res.json({
+        "k1":"data1",
+        "k2":"data2",
+        "k3":"data3"
+    });
+});
+
+
+app.post("/login", function(req, res) {  
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find((u)=> {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
 });
 
 app.use(express.static('frontend'));
+
+app.use(function(req, res, next) {
+    res.sendFile(__dirname + "/frontend/index.html");
+})
 
 reloadServer = reload(app);
 watch.watchTree(__dirname + "/frontend", function (f, curr, prev) {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
