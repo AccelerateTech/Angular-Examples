4ccb7b1eb97386a42dbbc8570d9d08a714a74f25
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2017-12-16 01:03:01 +0800

    Removed extra line and fields

--- backend/frontend/src/app/facebook-auth.service.ts
+++ backend/frontend/src/app/facebook-auth.service.ts
@@ -6,30 +6,17 @@ import { environment } from '../environments/environment';
 
 @Injectable()
 export class FacebookAuthService {
-    token: string = null;
-
     constructor(private router: Router,private oauthService: OAuthService){
-          // Login-Url
           this.oauthService.loginUrl = "https://www.facebook.com/v2.11/dialog/oauth"
-          
           this.oauthService.redirectUri = window.location.origin + "/auth/facebook/callback";
-          
           this.oauthService.clientId = environment.facebookAppId;
-          
           this.oauthService.scope = "email user_friends manage_pages";
-          
           this.oauthService.setStorage(localStorage);
-          
     }
 
     logIn(){
         this.oauthService.initImplicitFlow();
     }
-
-    isAuthenticated(){
-        return this.token != null;
-    }
-
     logOut(){
         this.oauthService.logOut();
     }
